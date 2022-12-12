5879efb0032aa1940d10ee21a31d6795dec69ca6
Soft-wa.re <my@Soft-wa.re>
2022-12-12 12:56:31 -0500

    copy pluralsight profile

---
+++ setupHostUbuntu.sh
@@ -0,0 +1,9 @@
+sudo apt-get install ruby ruby-dev
+sudo gem install bundler
+BUNDLE_ALLOW_BUNDLER_DEPENDENCY_CONFLICTS=true bundler install
+
+sudo gem install http_parser.rb -v '0.6.0' --source 'https://rubygems.org/'
+
+sudo gem install github-pages
+
+sudo gem install jekyll-theme-hacker
